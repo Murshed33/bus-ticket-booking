86b7978f6619985505a8b6f5803b273c2e05e4a5
Murshed-33 <iammurshedali@gmail.com>
2025-12-30 12:42:12 +0600

    Add ticket & confirm ticket fxml added by neesa

--- src/main/java/com/sohojbd/busticketbooking/busticketmanagement/Main.java
+++ src/main/java/com/sohojbd/busticketbooking/busticketmanagement/Main.java
@@ -1,17 +1,27 @@
 package com.sohojbd.busticketbooking.busticketmanagement;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-        public void start(Stage stage) throws Exception{
-            FXMLLoader loader= new FXMLLoader(Main.class.getResource("login.fxml"));
-            Scene scene= new Scene(loader.load(),600,500);
-            stage.setTitle("---Bus Ticket Booking---");
-            stage.setScene(scene);
-            stage.show();
-        }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+        FXMLLoader loader = new FXMLLoader(
+                getClass().getResource(
+                        "/com/sohojbd/busticketbooking/busticketmanagement/add_ticket.fxml"
+                )
+        );
+
+        Scene scene = new Scene(loader.load(), 800, 600);
+        stage.setTitle("Bus Ticket Management");
+        stage.setScene(scene);
+        stage.show();
+    }
+
     public static void main(String[] args) {
         launch();
     }
